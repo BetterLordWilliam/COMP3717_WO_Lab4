95a1f4fd319337175f803dcb393a3525c48f887a
LordWilliam <willjordan05@icloud.com>
2025-02-03 11:40:33 -0800

    WO: Started implementing race and mission type selection. Maybe will finish. Submitting the current implementation (basic implementation).

--- app/src/main/java/com/example/willotterbeinlab4/Main.kt
+++ app/src/main/java/com/example/willotterbeinlab4/Main.kt
@@ -5,6 +5,37 @@ package com.example.willotterbeinlab4
 * A01372608
 * */
 
+//val RACES_STRINGS = mapOf(
+//    1 to "Dwarf",
+//    2 to "Elf"
+//)
+//val MISSION_STRINGS = mapOf(
+//    1 to "Gather",
+//    2 to "Hunt"
+//)
+//
+//fun selectRace(): Int {
+//    while(true) {
+//        println("Select the race for the mission")
+//        for (i in RACES_STRINGS.keys) {
+//            println("${i}:\t${RACES_STRINGS[i]}")
+//        }
+//        try {
+//            print("Type your selection ")
+//            val selection: Int = readln().toInt()
+//            if (selection !in RACES_STRINGS.keys)
+//                throw NumberFormatException("Not in valid range 1..2")
+//            return selection
+//        } catch (e: NumberFormatException) {
+//            println("Invalid input: ${e.message}")
+//        }
+//    }
+//}
+
+fun selectMission(): Int {
+    return 0
+}
+
 fun main() {
     val myDwarf = Dwarf()
     val myElf = Elf()
@@ -12,6 +43,8 @@ fun main() {
     val myMissionManager = MissionManager()
     val mission1 = Gather(myDwarf)
     val mission2 = Hunt(myElf)
+
+    // selectRace()
 
     myMissionManager.select(mission2, object : MissionListener {
         override fun missionStart(minion: Minion) {
